937c3ceb765b2c5ae8ff70154b8674692f2ff975
AzureAD\고명민 <miung222@gmail.com>
2024-04-01 22:51:30 +0900

    helloworld.proto를 활용하는 GrpcHelloService로 수정

--- flutter_grpc_client/lib/src/service/grpc_service.dart
+++ flutter_grpc_client/lib/src/service/grpc_service.dart
@@ -1,27 +1,33 @@
+import 'package:flutter_grpc_client/protos/helloworld.pbgrpc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:grpc/grpc.dart';
 
-final grpcServiceProvider =
-    NotifierProvider<GrpcService, ClientChannel>(GrpcService.new);
+final grpcHelloServiceProvider =
+    NotifierProvider<GrpcHelloService, HelloClient>(GrpcHelloService.new);
 
-class GrpcService extends Notifier<ClientChannel> {
+class GrpcHelloService extends Notifier<HelloClient> {
   @override
-  ClientChannel build() => ClientChannel(
-        "localhost",
-        port: 50051,
-        options:
-            const ChannelOptions(credentials: ChannelCredentials.insecure()),
-      );
+  HelloClient build() {
+    _channel = ClientChannel(
+      "localhost",
+      port: 50051,
+      options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+    );
 
-  ClientChannel get channel => state;
+    return HelloClient(_channel);
+  }
+
+  late ClientChannel _channel;
+  HelloClient get helloClient => state;
 
   void updateChannel(String host, int port) {
-    state.shutdown(); // 기존 채널 종료
+    _channel.shutdown(); // 기존 채널 종료
     // 새로운 채널로 상태 업데이트
-    state = ClientChannel(
+    _channel = ClientChannel(
       host,
       port: port,
       options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
     );
+    state = HelloClient(_channel);
   }
 }
